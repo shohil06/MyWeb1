33511f4cf5872f2d8d15b579d7245fb9b35214a1
shohil06 <shohil.06@gmail.com>
2016-07-13 14:31:49 +0530

    changes commited to Bootstrap 3.3.6

--- includes/js/script.js
+++ includes/js/script.js
@@ -10,10 +10,17 @@ Notes:	 Hand coded for Udemy.com
 */
 
 $( function() {
-$('#Me').click(function(e) {
+$('#Me').click(function(shohil) {
 
-		e.preventDefault();
+		shohil.preventDefault();
 
 		$('#success').slideDown();
 });
-} );
+$('a.pop').click(function(n) {
+	n.preventDefault();
+});
+$('a.pop').popover();
+// $('a#href12').tooltip();
+$('a#shohil').tooltip();
+// $('a.href12').tooltip();
+});
